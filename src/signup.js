3f144221010a29d15a207bb6aec805f5a9b2c7e9
Alexey V Paramonov <me@paramonov.ru>
2017-03-11 20:47:51 +0300

    Signup form validation

--- src/signup.js
+++ src/signup.js
@@ -1,12 +1,22 @@
 (function () {
     let signupBtn = document.getElementById("signup-submit");
-    signupBtn.addEventListener('click', function (e) {
-        console.log('button clicked!', arguments);
+    signupBtn.addEventListener('click', (e) => {
+
         e.preventDefault();
 
-        var form = document.getElementById('signup-form');
-        var data = new FormData(form);
-        var req = new XMLHttpRequest();
+        const form = document.getElementById('signup-form');
+        const submitBtn = document.getElementById('signup-submit');
+        let data = new FormData(form);
+        let req = new XMLHttpRequest();
+
+        // Hide all errors
+        let errorMessages = document.querySelectorAll('div[id^="form-error-"]');
+        for (let errMsgEl of errorMessages) {
+            errMsgEl.style.visibility = 'hidden';
+        }
+
+        // Disable submit while request is running
+        submitBtn.disabled = true;
 
         req.open('POST', API_BASE + '/users/');
         req.onreadystatechange = function (e) {
@@ -14,15 +24,29 @@
                 // User created
                 if (req.status === 201) {
                     let response = JSON.parse(req.responseText);
-                    if(response.token){
+                    if (response.token) {
                         window.localStorage.setItem('token', response.token);
                         window.location.href = "/account";
                     }
+                // Request returned with an error
                 } else {
-                    console.log("Error", req.statusText);
+                    let responseData = JSON.parse(req.response);
+                    for (let field of Object.keys(responseData)) {
+
+                        let errorCodes = responseData[field];
+                        for (let errCode of errorCodes) {
+                            let errMsgEl = document.getElementById(`form-error-${field}-${errCode}`);
+                            if (errMsgEl) {
+                                errMsgEl.style.visibility = 'visible';
+                            }
+                        }
+
+                    }
+                    submitBtn.disabled = false;
                 }
             }
         };
         req.send(data);
+
     }, false);
 }());
