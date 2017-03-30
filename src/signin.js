d8b516209b0eecc84a0acf88a10c805b0f3e1081
Alexey V Paramonov <me@paramonov.ru>
2017-03-30 18:12:11 +0300

    Signin

--- src/signin.js
+++ src/signin.js
@@ -18,7 +18,7 @@
         // Disable submit while request is running
         signinBtn.disabled = true;
 
-        req.open('POST', API_BASE + '/users/login');
+        req.open('POST', API_BASE + '/api-token-auth/');
         req.onreadystatechange = function (e) {
             if (req.readyState === 4) {
                 // Logged in uccessfully
@@ -28,14 +28,23 @@
                         window.localStorage.setItem('token', response.token);
                         window.location.href = "/account";
                     }
+                    else {
+                        let errMsgEl = document.getElementById("form-error-no-token");
+                        if (errMsgEl) {
+                            errMsgEl.style.visibility = 'visible';
+                        }
+                        signinBtn.disabled = false;
+                    }
                 // Request returned with an error
                 } else {
                     let responseData = JSON.parse(req.response);
                     for (let field of Object.keys(responseData)) {
-
                         let errorCodes = responseData[field];
                         for (let errCode of errorCodes) {
+                            
+                            errCode = errCode.replace(/\s/g, '-').replace(/\./g, '').toLowerCase();
                             let errMsgEl = document.getElementById(`form-error-${field}-${errCode}`);
+                            console.log(`form-error-${field}-${errCode}`);
                             if (errMsgEl) {
                                 errMsgEl.style.visibility = 'visible';
                             }
