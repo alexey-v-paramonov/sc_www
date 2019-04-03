696f7b7f06158bc6152099b117fa381feefc9852
Alexey V Paramonov <me@paramonov.ru>
2019-04-03 11:03:09 +0300

    Update packages, disable js linter

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,12 +26,12 @@ module.exports = {
     */
     extend (config, { isDev, isClient }) {
       if (isDev && isClient) {
-        config.module.rules.push({
+        /*config.module.rules.push({
           enforce: 'pre',
           test: /\.(js|vue)$/,
           loader: 'eslint-loader',
           exclude: /(node_modules)/
-        })
+        })*/
       }
     }
   }
