9963817ca01642cec11b66bdb02afd80163f0e67
Alexey V Paramonov <me@paramonov.ru>
2019-12-28 22:41:39 +0300

    Signup component

--- components/Signup.vue
+++ components/Signup.vue
@@ -1,6 +1,21 @@
 <template>
 <div>
 <h4>signup</h4>
-<input type="email" name="email" placeholder="email">
+<label class="col-xl-2 col-form-label">{{$t('email')}}</label>
+<input class="form-control" :class="{'is-invalid': errors.has('email')}"
+    type="email" name="email" placeholder="email" v-validate="'required|email'" v-model="signup.email">
+
 </div>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                signup: {
+                    email: ''
+                }
+            }
+        }
+    };
+</script>
