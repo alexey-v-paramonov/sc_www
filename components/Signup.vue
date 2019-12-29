6a434e94685b8fd435d1c040dbd2636431acfd08
Alexey V Paramonov <me@paramonov.ru>
2019-12-29 17:00:53 +0300

    Vee validate 3 changes

--- components/Signup.vue
+++ components/Signup.vue
@@ -1,15 +1,22 @@
 <template>
 <div>
 <h4>signup</h4>
-<label class="col-xl-2 col-form-label">{{$t('email')}}</label>
-<input class="form-control" :class="{'is-invalid': errors.has('email')}"
-    type="email" name="email" placeholder="email" v-validate="'required|email'" v-model="signup.email">
-
+    <ValidationProvider rules="required|email" v-slot="{ errors }">
+        <label class="col-xl-2 col-form-label">{{$t('email')}}</label>
+        <input class="form-control" :class="{'is-invalid': errors.has('email')}"
+            type="email" name="email" placeholder="email" v-model="signup.email">
+        <span>{{ errors[0] }}</span>
+    </ValidationProvider>
 </div>
 </template>
 
 <script>
+    import { ValidationProvider } from 'vee-validate';
+
     export default {
+        components: {
+            ValidationProvider
+        },
         data() {
             return {
                 signup: {
--- plugins/vee-validate.js
+++ plugins/vee-validate.js
@@ -1,13 +1,13 @@
-import Vue from 'vue';
-import VeeValidate from 'vee-validate';
-import VueI18n from 'vue-i18n'
+// import Vue from 'vue';
+// import VeeValidate from 'vee-validate';
+// import VueI18n from 'vue-i18n'
 
-export default ({ app }) => {
-  Vue.use(VeeValidate, {
-    inject: true,
-    i18n: app.i18n, // the vue-i18n plugin instance
-    i18nRootKey: 'validations',
-    fieldsBagName: 'formFields'
-    //locale: 'en'
-  });
-}
+// export default ({ app }) => {
+//   Vue.use(VeeValidate, {
+//     inject: true,
+//     i18n: app.i18n, // the vue-i18n plugin instance
+//     i18nRootKey: 'validations',
+//     fieldsBagName: 'formFields'
+//     //locale: 'en'
+//   });
+// }
