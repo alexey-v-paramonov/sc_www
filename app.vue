21a25546811a7d8ddb116ecf4e3568a0f0a2faf9
Alexey V Paramonov <me@paramonov.ru>
2023-02-04 14:38:58 +0300

    Wip

--- app.vue
+++ app.vue
@@ -1,5 +1,27 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <v-app>
+    <v-navigation-drawer v-model="drawer">
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Nuxt 3 & Vuetify 3 Template</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <v-btn>The Button</v-btn>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
+
+<script lang="ts">
+export default {
+  data: () => ({
+    drawer: null,
+  }),
+};
+</script>
