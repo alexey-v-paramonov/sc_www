ebffd2d0828700720f15ac626fed95fcd769e8ea
Alexey V Paramonov <me@paramonov.ru>
2023-03-19 18:15:41 +0300

    Translations

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -3,21 +3,21 @@
         <v-row no-gutters md="6">
             <v-col>&nbsp;</v-col>
             <v-col md="6">
-                <h1><p>{{ $t('welcome') }}</p> Login</h1>
+                <h1>{{ $t('login') }}</h1>
                 <v-form @submit.prevent>
-                    <v-text-field v-model="firstName" :rules="rules" label="Email"></v-text-field>
+                    <v-text-field v-model="email" :rules="rules" :label="$t('email')"></v-text-field>
                     <v-text-field
-            v-model="password"
-            :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-            :rules="[rules.required, rules.min]"
-            :type="show1 ? 'text' : 'password'"
-            name="input-10-1"
-            label="Normal with hint text"
-            hint="At least 8 characters"
-            counter
-            @click:append="show1 = !show1"
-          ></v-text-field>
-                              <v-btn type="submit" block class="mt-2">Submit</v-btn>
+                        v-model="password"
+                        :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                        :rules="[rules.required, rules.min]"
+                        :type="show1 ? 'text' : 'password'"
+                        name="input-10-1"
+                        :label="$t('password')"
+                        hint="At least 8 characters"
+                        counter
+                        @click:append="show1 = !show1"
+                    ></v-text-field>
+                    <v-btn type="submit" block class="mt-2">{{ $t('login') }}</v-btn>
                 </v-form>
             </v-col>
         </v-row>
