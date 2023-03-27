9ea23908db23309715bade147be40ce9523c03ff
Alexey V Paramonov <me@paramonov.ru>
2023-03-27 22:41:11 +0300

    Validation

--- pages/signup/index.vue
+++ pages/signup/index.vue
@@ -4,8 +4,10 @@
             <v-col>&nbsp;</v-col>
             <v-col md="6">
                 <h1>{{ $t('signup') }}</h1>
-                <Form as="v-form" @submit.prevent>
-                    <v-text-field v-model="email" type="email" :rules="rules" :label="$t('email')"></v-text-field>
+                <Form as="v-form" @submit="onSubmit">
+                    <Field name="email" as="v-text-field" v-model="email" type="email" :rules="validateEmail"  :label="$t('email')"></Field>
+                    1<ErrorMessage name="email" />2
+
                     <v-text-field
                         v-model="password"
                         :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
@@ -17,7 +19,11 @@
                         counter
                         @click:append="show1 = !show1"
                     ></v-text-field>
+                    <error-message name="email">123</error-message>
+                    <v-btn type="submit" block class="mt-2">{{ $t('create_account') }}</v-btn>
+                    <!--
                     <v-btn type="submit" @click="signUp()" block class="mt-2">{{ $t('create_account') }}</v-btn>
+                    -->
                 </Form>
             </v-col>
         </v-row>
@@ -25,7 +31,13 @@
 </template>
   
 <script>
+import { Form, Field, ErrorMessage } from 'vee-validate';
+
 export default {
+  components: {
+    Form,
+    Field,
+  },  
     data () {
       return {
         show1: false,
@@ -42,6 +54,22 @@ export default {
     created(){
     },
     methods: {
+      validateEmail(value) {
+        // if the field is empty
+        if (!value) {
+          return 'This field is required';
+        }
+        // if the field is not a valid email
+        const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+        if (!regex.test(value)) {
+          return 'This field must be a valid email';
+        }
+        // All is good
+        return true;
+        },      
+      onSubmit(values) {
+        console.log('Submitting :(', values);
+      },        
       async signUpRequest() {
         const config = useRuntimeConfig();
         return await $fetch(`${config.public.baseURL}/users/`, { 
--- plugins/veeValudate.ts
+++ plugins/veeValudate.ts
@@ -1,5 +1,12 @@
-import { Form, Field, ErrorMessage } from 'vee-validate';
+import { configure, defineRule, Form, Field, ErrorMessage } from 'vee-validate'
+
+import AllRules from '@vee-validate/rules'
+
 
 export default defineNuxtPlugin(nuxtApp => {
-    nuxtApp.vueApp.component(Form);
+    Object.entries(AllRules).forEach(([id, validator]) => {
+        defineRule(id, validator)
+      })    
+    nuxtApp.vueApp.component("Form", Form);
+    nuxtApp.vueApp.component("ErrorMessage", ErrorMessage);
 })
