dabf0030cfb1f369d1f8e5196f445cc9c2213e89
Alexey V Paramonov <me@paramonov.ru>
2023-04-01 14:21:31 +0300

    Validation local messages

--- plugins/veeValudate.ts
+++ plugins/veeValudate.ts
@@ -1,12 +1,22 @@
 import { configure, defineRule, Form, Field, ErrorMessage } from 'vee-validate'
+import { localize, setLocale } from '@vee-validate/i18n';
+import AllRules from '@vee-validate/rules';
 
-import AllRules from '@vee-validate/rules'
-
+import en from '@vee-validate/i18n/dist/locale/en.json';
+import ru from '@vee-validate/i18n/dist/locale/ru.json';
 
 export default defineNuxtPlugin(nuxtApp => {
     Object.entries(AllRules).forEach(([id, validator]) => {
         defineRule(id, validator)
-      })    
+    })
+    configure({
+      generateMessage: localize({
+        en,
+        ru,
+      }),
+    });
+
+    setLocale('ru');
     //nuxtApp.vueApp.component("Form", Form);
     //nuxtApp.vueApp.component("Field", Field);
     //nuxtApp.vueApp.component("ErrorMessage", ErrorMessage);
