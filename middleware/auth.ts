59212a7b1d64fc535a70e17b880ee89f0748a4c2
Alexey V Paramonov <me@paramonov.ru>
2023-09-20 07:52:44 +0300

    Disable SSR to make store and auth middleware work

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,12 +1,9 @@
+import { useUserStore } from '@/stores/user.js';
+
 export default defineNuxtRouteMiddleware((to, from) => {
-    console.log("Auth middleare!");
-    // const auth = useState('auth')
-  
-    // if (!auth.value.isAuthenticated) {
-    //   return navigateTo('/login')
-    // }
-  
-    // if (to.path !== '/dashboard') {
-    //   return navigateTo('/dashboard')
-    // }
+    const { user } = useUserStore();
+
+    if (!user.isAuthenticated) {
+        return navigateTo('/login')
+    }
 })
