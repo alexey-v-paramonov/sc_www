6a1c4cd1739aa5a337907cdf5b145013640b5fc7
Alexey V Paramonov <me@paramonov.ru>
2023-10-03 18:10:48 +0300

    IP address validation with localized error message

--- plugins/veeValidate.ts
+++ plugins/veeValidate.ts
@@ -9,19 +9,23 @@ import ru from '@vee-validate/i18n/dist/locale/ru.json';
 
 
 export default defineNuxtPlugin(nuxtApp => {
-    Object.entries(AllRules).forEach(([id, validator]) => {
-        defineRule(id, validator);
-    });
+  const $t = nuxtApp.$i18n.t;
+  Object.entries(AllRules).forEach(([id, validator]) => {
+    defineRule(id, validator);
+  });
 
-    defineRule('ip', value => {
-      return (/((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})(\.((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})){3}/g.test(value))
-    });
+  defineRule('ip', value => {
+    if (!/((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})(\.((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})){3}/g.test(value)) {
+      return $t('errors.ip_invalid');
+    }
+    return true;
+  });
 
-    configure({
-      generateMessage: localize({
-        en,
-        ru,
-      }),
-    });
-    
+  configure({
+    generateMessage: localize({
+      en,
+      ru,
+    }),
+  });
+
 })
