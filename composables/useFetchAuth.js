80616a4b0335b2281e0735405a567514b69dc356
Alexey V Paramonov <me@paramonov.ru>
2023-10-27 10:02:47 +0300

    Upgrade packages

--- composables/useFetchAuth.js
+++ composables/useFetchAuth.js
@@ -4,6 +4,7 @@ import { useUserStore } from '~/stores/user'
 export const useFetchAuth = (url, opts) => {
     const { user } = useUserStore();
 
+    // timeout: 30000
     const headers = {
       ...(opts?.headers || {}),
       ...(user.token && { Authorization: `Token ${user.token}` }),
