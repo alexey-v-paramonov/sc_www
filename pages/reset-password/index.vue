21af1f36eed17e7dad1b5ad1c89e069e86b7d7ca
Alexey V Paramonov <me@paramonov.ru>
2023-12-24 23:29:41 +0300

    Password reset page

--- pages/reset-password/index.vue
+++ pages/reset-password/index.vue
@@ -5,16 +5,30 @@
         <v-row no-gutters md="6">
           <v-col>&nbsp;</v-col>
           <v-col md="6">
-            <h1>{{ $t('pass_reset_email_addr') }}</h1>
-            <v-form @submit.prevent="resetPassword">
+            <h1>{{ resetDone ? $t('password_reset.check_inbox') : $t('password_reset.email_address') }}</h1>
+            <v-form @submit.prevent="resetPassword" v-if="!resetDone">
 
                 <v-text-field v-model="email.value.value" type="email" :error-messages="email.errorMessage.value"
                 :label="$t('email')"></v-text-field>
 
-                <NuxtLink to="/login" class="text-body-2 font-weight-regular">{{ $t('pass_reset_back_to_login') }}</NuxtLink>
+                <NuxtLink to="/login" class="text-body-2 font-weight-regular">{{ $t('password_reset.back_to_login') }}</NuxtLink>
   
-              <v-btn type="submit" :disabled="isPassResetSubmitting" block class="mt-2">{{ isPassResetSubmitting ? $t('loading') : $t('pass_reset_cta') }}</v-btn>
+              <v-btn type="submit" :disabled="isPassResetSubmitting" block class="mt-2">{{ isPassResetSubmitting ? $t('loading') : $t('password_reset.cta') }}</v-btn>
             </v-form>
+            <div class="mt-2" v-if="resetDone">
+              {{ $t('password_reset.check_inbox_email') }} <strong>{{ email.value.value }}</strong>
+              <v-card
+                :title="$t('password_reset.check_inbox_hint_head')"
+                :text="$t('password_reset.check_inbox_hint_text')"
+                prepend-icon="mdi-alert"
+              >
+              <v-card-actions>
+                <v-btn @click="resetDone = false">
+                  {{ $t('password_reset.resend') }}
+                </v-btn>
+              </v-card-actions>
+            </v-card>              
+            </div>
             <div class="mt-2">
               <p class="text-body-2">{{ $t('account_creation_hint') }} <NuxtLink to="/signup">{{ $t('signup') }}</NuxtLink>
               </p>
@@ -27,18 +41,23 @@
     
   <script setup>
   import { useField, useForm } from 'vee-validate';
-  import { ref } from 'vue'
+  import { ref } from 'vue';
+  const { locale, t } = useI18n();
+
   
   definePageMeta({
     layout: false,
   });
+
+  const resetDone = ref(false);
     
   async function resetPasswordRequest(data) {
     const config = useRuntimeConfig();
-    return await useFetch(`${config.public.baseURL}/api-token-auth/`, {
+    return await useFetch(`${config.public.baseURL}/password_reset/`, {
       method: 'POST',
       body: {
         'email': data.email,
+        'lang': locale
       }
     });
   }
@@ -51,12 +70,13 @@
     const response = await resetPasswordRequest(values);
     const error = response.error.value;
     if (!error) {
-        return;
+      resetDone.value = true;
+      return;
     }
     //console.log("Errors: ", error.data)
     let errors = {};
     for (const error_field in error.data) {
-        errors[error_field] = t(`${error_field}.errors.${error.data[error_field]}`);
+        errors[error_field] = t(`password_reset.errors.${error_field}.${error.data[error_field]}`);
     }
     setErrors(errors);
 
