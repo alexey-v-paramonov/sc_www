a487da90bddb0dabc4cfa0be72696df385c4d540
Alexey V Paramonov <me@paramonov.ru>
2023-12-27 22:00:00 +0300

    Password reset form improvements

--- pages/reset-password/confirm/index.vue
+++ pages/reset-password/confirm/index.vue
@@ -5,9 +5,9 @@
         <v-row no-gutters md="6">
           <v-col>&nbsp;</v-col>
           <v-col md="6">
-            <h1>{{ $t('password_reset.new_pass') }}</h1>
+            <h1 v-if="!resetConfirmDone">{{ $t('password_reset.new_pass') }}</h1>
             <!-- Confirm form -->
-            <v-form @submit.prevent="resetConfirmPassword" :disabled="isPassResetConfirmSubmitting">
+            <v-form v-if="!resetConfirmDone" @submit.prevent="resetConfirmPassword" :disabled="isPassResetConfirmSubmitting">
 
               <v-text-field v-model="password.value.value" :append-icon="showPass ? 'mdi-eye' : 'mdi-eye-off'"
               :type="showPass ? 'text' : 'password'" name="password" :label="$t('password')" :hint="$t('chars_min_8')"
@@ -15,24 +15,23 @@
 
               <NuxtLink to="/login" class="text-body-2 font-weight-regular">{{ $t('password_reset.back_to_login') }}</NuxtLink>
 
-              <v-btn type="submit" :disabled="isPassResetConfirmSubmitting" block class="mt-2">{{ isPassResetSubmitting ? $t('loading')  : $t('password_reset.cta') }}</v-btn>
+              <v-btn type="submit" :disabled="isPassResetConfirmSubmitting" block class="mt-2">{{ isPassResetConfirmSubmitting ? $t('loading')  : $t('password_reset.cta') }}</v-btn>
             </v-form>
 
             <div class="mt-2" v-if="resetConfirmDone">
-              {{ $t('password_reset.check_inbox_email') }} <strong>{{ email.value.value }}</strong>
               <v-card
-                :title="$t('password_reset.check_inbox_hint_head')"
-                :text="$t('password_reset.check_inbox_hint_text')"
-                prepend-icon="mdi-alert"
+                :title="$t('password_reset.done_title')"
+                :text="$t('password_reset.done_text')"
+                prepend-icon="mdi-check"
               >
               <v-card-actions>
-                <v-btn @click="resetDone = false">
-                  {{ $t('password_reset.resend') }}
+                <v-btn @click="goToLogin()">
+                  {{ $t('password_reset.back_to_login') }}
                 </v-btn>
               </v-card-actions>
             </v-card>              
             </div>
-            <div class="mt-2">
+            <div class="mt-2" v-if="!resetConfirmDone">
               <p class="text-body-2">{{ $t('account_creation_hint') }} <NuxtLink to="/signup">{{ $t('signup') }}</NuxtLink>
               </p>
             </div>
@@ -53,11 +52,16 @@
 
   const { locale, t } = useI18n();
   const route = useRoute();
+  const router = useRouter();
   const showPass = ref(false);
 
   const resetToken = ref(route.query.t || '')
   const resetUID = ref(route.query.u || '')
   const resetConfirmMode = ref(Boolean(resetToken.value) && Boolean(resetUID.value));
+
+  if(!resetConfirmMode.value){
+    router.push("/reset-password");
+  }
   
   const resetConfirmDone = ref(false);
 
@@ -89,8 +93,10 @@
         errors[error_field] = t(`password_reset.errors.${error_field}.${error.data[error_field]}`);
     }
     setErrors(errors);
-
-  
   });
+
+  function goToLogin(){
+    router.push("/login");
+  }
   
   </script>
