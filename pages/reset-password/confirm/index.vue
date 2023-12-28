a9c498792e9e62d47a913ad675c5726a5a70fe1a
Alexey V Paramonov <me@paramonov.ru>
2023-12-28 22:10:29 +0300

    Password reset error handling

--- pages/reset-password/confirm/index.vue
+++ pages/reset-password/confirm/index.vue
@@ -5,9 +5,10 @@
         <v-row no-gutters md="6">
           <v-col>&nbsp;</v-col>
           <v-col md="6">
-            <h1 v-if="!resetConfirmDone">{{ $t('password_reset.new_pass') }}</h1>
+            <h1 v-if="!resetConfirmDone && !badToken">{{ $t('password_reset.new_pass') }}</h1>
+            
             <!-- Confirm form -->
-            <v-form v-if="!resetConfirmDone" @submit.prevent="resetConfirmPassword" :disabled="isPassResetConfirmSubmitting">
+            <v-form v-if="!resetConfirmDone && !badToken" @submit.prevent="resetConfirmPassword" :disabled="isPassResetConfirmSubmitting">
 
               <v-text-field v-model="password.value.value" :append-icon="showPass ? 'mdi-eye' : 'mdi-eye-off'"
               :type="showPass ? 'text' : 'password'" name="password" :label="$t('password')" :hint="$t('chars_min_8')"
@@ -18,18 +19,31 @@
               <v-btn type="submit" :disabled="isPassResetConfirmSubmitting" block class="mt-2">{{ isPassResetConfirmSubmitting ? $t('loading')  : $t('password_reset.cta') }}</v-btn>
             </v-form>
 
+            <div class="mt-2" v-if="badToken">
+              <v-card
+                  :title="$t('password_reset.token_is_invalid')"
+                  :text="$t('password_reset.token_is_invalid_text')"
+                  prepend-icon="mdi-alert"
+                >
+                <v-card-actions>
+                  <v-btn @click="goToPasswordReset()">
+                    {{ $t('password_reset.cta') }}
+                  </v-btn>
+                </v-card-actions>
+              </v-card>              
+            </div>
             <div class="mt-2" v-if="resetConfirmDone">
               <v-card
                 :title="$t('password_reset.done_title')"
                 :text="$t('password_reset.done_text')"
                 prepend-icon="mdi-check"
               >
-              <v-card-actions>
-                <v-btn @click="goToLogin()">
+                <v-card-actions>
+                  <v-btn @click="goToLogin()">
                   {{ $t('password_reset.back_to_login') }}
-                </v-btn>
-              </v-card-actions>
-            </v-card>              
+                  </v-btn>
+                </v-card-actions>
+              </v-card>              
             </div>
             <div class="mt-2" v-if="!resetConfirmDone">
               <p class="text-body-2">{{ $t('account_creation_hint') }} <NuxtLink to="/signup">{{ $t('signup') }}</NuxtLink>
@@ -50,7 +64,7 @@
     layout: false,
   });
 
-  const { locale, t } = useI18n();
+  const { t } = useI18n();
   const route = useRoute();
   const router = useRouter();
   const showPass = ref(false);
@@ -58,6 +72,7 @@
   const resetToken = ref(route.query.t || '')
   const resetUID = ref(route.query.u || '')
   const resetConfirmMode = ref(Boolean(resetToken.value) && Boolean(resetUID.value));
+  let badToken = ref(false);
 
   if(!resetConfirmMode.value){
     router.push("/reset-password");
@@ -73,7 +88,6 @@
         'password': data.password,
         'token': resetToken.value,
         'uid': resetUID.value,
-        'lang': locale,
       }
     });
   }
@@ -82,19 +96,32 @@
   const password = useField("password", "required|min:8");
 
   const resetConfirmPassword = handleSubmit(async values => {
+    badToken.value = false;
+
     const response = await resetConfirmPasswordRequest(values);
     const error = response.error.value;
     if (!error) {
       resetConfirmDone.value = true;
       return;
     }
+
+    if (error.data['non_field_errors'] == 'token_invalid') {
+      badToken.value = true;
+      return;
+    }
+
     let errors = {};
     for (const error_field in error.data) {
-        errors[error_field] = t(`password_reset.errors.${error_field}.${error.data[error_field]}`);
+      // Put all errors into the password field
+      error_field = 'password';
+      errors[error_field] = t(`password_reset.errors.${error_field}.${error.data[error_field]}`);
     }
     setErrors(errors);
   });
 
+  function goToPasswordReset(){
+    router.push("/reset-password/");
+  }
   function goToLogin(){
     router.push("/login");
   }
