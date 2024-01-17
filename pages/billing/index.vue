639dee5c8e0a924c1b17b234e3a25e93fc9dfcac
Alexey V Paramonov <me@paramonov.ru>
2024-01-17 08:09:18 +0300

    Account balance

--- pages/billing/index.vue
+++ pages/billing/index.vue
@@ -1,11 +1,15 @@
 <template>
     <v-container>
-      <v-row no-gutters md="6">
-        <v-col>Billing</v-col>
+      <v-row no-gutters md="12">
+        <v-col cols="6" class="text-center" >{{ $t('billing.balance') }}</v-col>
+        <v-col cols="6" class="text-center" >{{stateUser.user.userData.balance}} {{ $t('currency')}}</v-col>
       </v-row>
     </v-container>
   </template>
   
 <script setup>
+
+import { useUserStore } from '~/stores/user'
+const stateUser = useUserStore()
 
 </script>
