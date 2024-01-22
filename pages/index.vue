53d8895438257c8be88cb223e94e9c82499ee727
Alexey V Paramonov <me@paramonov.ru>
2024-01-22 11:08:41 +0300

    List all services on the main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,78 @@
 <template>
-    <v-container>
-      <v-row no-gutters md="6">
-        <v-col>User area <NuxtLink to="/radio/wizard">{{ $t('radio.wizard') }}</NuxtLink>
-        {{ self_hosted_radios.length }}
-        {{ hosted_radios.length }}
+  <v-container>
+    <v-row>
+      <!-- Self hosted radio -->
+      <v-col cols="6" class="text-center">
+        <NuxtLink to="/radio">
+          <v-badge :content="self_hosted_radios.length">
+            <v-card class="mx-auto" width="400">
+              <v-card-text>
+                <div class="text-center"><v-icon icon="mdi-server-network" size="200"></v-icon></div>
+                <div class="text-center">{{ $t('radios.self_hosted.title') }}</div>
+              </v-card-text>
+            </v-card>
+          </v-badge>
+        </NuxtLink>
+      </v-col>
 
-        {{ android_apps.length }}
-        {{ ios_apps.length }}
+      <!-- Hosted radio -->
+      <v-col cols="6" class="text-center">
+        <NuxtLink to="/radio">
+          <v-badge :content="hosted_radios.length">
+            <v-card class="mx-auto" width="400">
 
-        
-        </v-col>
-      </v-row>
-    </v-container>
-  </template>
+              <v-card-text>
+                <div class="text-center"><v-icon icon="mdi-radio" size="200"></v-icon></div>
+                <div class="text-center">{{ $t('radios.hosted.title') }}</div>
+              </v-card-text>
+            </v-card>
+          </v-badge>
+        </NuxtLink>
+      </v-col>
+    </v-row>
+
+    <v-row no-gutters md="12">
+      <v-col cols="12" class="text-center"><NuxtLink to="/radio/wizard">{{ $t('radio.wizard') }}</NuxtLink></v-col>
+    </v-row>
+
+    <v-row>
+
+      <!-- Android Apps -->
+      <v-col cols="6" class="text-center">
+        <NuxtLink to="/apps/android">
+        <v-badge :content="android_apps.length">
+          <v-card class="mx-auto" width="400">
+
+            <v-card-text>
+              <div class="text-center"><v-icon icon="mdi-android" size="200"></v-icon></div>
+              <div class="text-center">{{ $t('nav_android') }}</div>
+            </v-card-text>
+          </v-card>
+        </v-badge>
+      </NuxtLink>
+      </v-col>
+
+      <!-- iOS Apps -->
+      <v-col cols="6" class="text-center">
+        <NuxtLink to="/apps/ios">
+        <v-badge :content="ios_apps.length">
+          <v-card class="mx-auto" width="400">
+
+            <v-card-text>
+              <div class="text-center"><v-icon icon="mdi-apple" size="200"></v-icon></div>
+              <div class="text-center">{{ $t('nav_ios') }}</div>
+            </v-card-text>
+          </v-card>
+        </v-badge>
+      </NuxtLink>
+      </v-col>
+    </v-row>
+    <v-row no-gutters md="12">
+      <v-col cols="12" class="text-center">Create new</v-col>
+    </v-row>
+
+  </v-container>
+</template>
   
 <script setup>
 import { useUserStore } from '@/stores/user.js';
@@ -30,10 +90,10 @@ let self_hosted_radios_loading = ref(false);
 let hosted_radios_loading = ref(false);
 
 let android_apps = ref([]);
-let android_apps_loading  = ref(false);
+let android_apps_loading = ref(false);
 
 let ios_apps = ref([]);
-let ios_apps_loading  = ref(false);
+let ios_apps_loading = ref(false);
 
 const config = useRuntimeConfig();
 
