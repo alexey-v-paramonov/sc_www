c8f59dbcab86c9900c04f247e19ab2afac848ed8
Alexey V Paramonov <me@paramonov.ru>
2024-01-29 10:33:00 +0300

    Add data loading and error handling

--- components/AppForm.vue
+++ components/AppForm.vue
@@ -1,10 +1,39 @@
 <template>
-    App form {{ platform }} 
+    <v-row no-gutters md="12" v-if="pending">
+        <v-col>
+            <v-progress-circular indeterminate></v-progress-circular>
+        </v-col>
+    </v-row>
+
+    <v-snackbar v-model="appRequesFailed" color="error">
+        {{ $t('app.request_failed') }}
+
+        <template v-slot:actions>
+            <v-btn color="white" variant="text" @click="appRequesFailed = false">
+                {{ $t('close') }}
+            </v-btn>
+        </template>
+    </v-snackbar>
 </template>
 
 
 <script setup>
-    const props = defineProps({platform: String, id: Number})
-    console.log("platform: ", props.platform, props.id)
+import { ref, reactive } from 'vue';
+
+const config = useRuntimeConfig();
+const props = defineProps({ platform: String, id: Number })
+// const router = useRouter();
+
+let appData = reactive({});
+let appRequesFailed = ref(false);
+
+const { data, pending, error } = await useFetchAuth(`${config.public.baseURL}/mobile_apps/${props.platform}/${props.id}/`);
+appData.value = data.value || {};
+
+if (error.value) {
+    appRequesFailed.value = true;
+    // router.push(`/apps/${props.platform}/`);
+}
+
 
 </script>
