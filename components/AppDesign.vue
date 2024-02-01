0bf748f84cc1a7a7769b5b2a8c41734461564688
Alexey V Paramonov <me@paramonov.ru>
2024-02-01 17:47:24 +0300

    Add design and colors section

--- components/AppDesign.vue
+++ components/AppDesign.vue
@@ -1,50 +1,105 @@
 <template>
-    123
     <v-container>
         <v-row no-gutters md="12">
-
             <v-col cols="6">
-                <v-form @submit.prevent="onAppDesignSubmit" :disabled="isSettingsSubmitting">
-                    <v-text-field v-model="title.value.value" type="color" :error-messages="title.errorMessage.value"
-                        :label="$t('app.title')" maxlength="30"></v-text-field>
+                <v-form @submit.prevent="onAppDesignSubmit" :disabled="isAppDesignSubmitting">
+
+                    {{ $t('app.design.bg_color') }}
+                    <v-color-picker v-model="bg_color.value.value" mode="rgb" :modes="['rgb',]"></v-color-picker>
+
+                    {{ $t('app.design.bg_color_gradient') }}
+                    <v-color-picker v-model="bg_color_gradient.value.value" mode="rgb" :modes="['rgb',]"></v-color-picker>
+
+                    {{ $t('app.design.font_color') }}
+                    <v-color-picker v-model="font_color.value.value" mode="rgb" :modes="['rgb',]"></v-color-picker>
+
+                    {{ $t('app.design.text_secondary_color') }}
+                    <v-color-picker v-model="text_secondary_color.value.value" mode="rgb" :modes="['rgb',]"></v-color-picker>
+
+                    {{ $t('app.design.button_text_color') }}
+                    <v-color-picker v-model="button_text_color.value.value" mode="rgb" :modes="['rgb',]"></v-color-picker>
+
+                    {{ $t('app.design.tabs_color') }}
+                    <v-color-picker v-model="tabs_color.value.value" mode="rgb" :modes="['rgb',]"></v-color-picker>
+
+                    {{ $t('app.design.tabs_icon_color') }}
+                    <v-color-picker v-model="tabs_icon_color.value.value" mode="rgb" :modes="['rgb',]"></v-color-picker>
+
+                    {{ $t('app.design.tabs_icon_selected_color') }}
+                    <v-color-picker v-model="tabs_icon_selected_color.value.value" mode="rgb" :modes="['rgb',]"></v-color-picker>
+
+                    {{ $t('app.design.main_theme_color') }}
+                    <v-color-picker v-model="main_theme_color.value.value" mode="rgb" :modes="['rgb',]"></v-color-picker>
+
+                    {{ $t('app.design.play_button_border_color') }}
+                    <v-color-picker v-model="play_button_border_color.value.value" mode="rgb" :modes="['rgb',]"></v-color-picker>
+
+                    {{ $t('app.design.volume_buttons_color') }}
+                    <v-color-picker v-model="volume_buttons_color.value.value" mode="rgb" :modes="['rgb',]"></v-color-picker>
+
+                    {{ $t('app.design.volume_bar_active_color') }}
+                    <v-color-picker v-model="volume_bar_active_color.value.value" mode="rgb" :modes="['rgb',]"></v-color-picker>
+
+                    {{ $t('app.design.volume_bar_inactive_color') }}
+                    <v-color-picker v-model="volume_bar_inactive_color.value.value" mode="rgb" :modes="['rgb',]"></v-color-picker>
 
                 </v-form>
             </v-col>
 
             <!-- Preview -->
             <v-col cols="6">
+                <div class="sticky-top">
+                Preview
+
+                <v-btn color="primary">{{ $t('app.design.choose_skin') }}</v-btn>
+
+                </div>
             </v-col>
 
         </v-row>
+        <v-row no-gutters md="12">
+            <v-col>
+                <v-btn type="submit" :disabled="isAppDesignSubmitting" block class="mt-2" color="primary">{{
+                    isAppDesignSubmitting ? $t('loading') : $t('save') }}</v-btn>
+            </v-col>
+
+        </v-row>
+
+
     </v-container>
 </template>
 
 <script setup>
 import { ref, reactive } from 'vue';
 import { useField, useForm } from 'vee-validate';
+// const emit = defineEmits(['AppInfoRefresh',])
+const props = defineProps({ platform: String, id: Number, appData: Object })
 
-const { handleSubmit, isSubmitting: isAppInfoSubmitting, setErrors } = useForm({
-    initialValues: {
-        // title: appData.value.title || '',
-        // email: appData.value.email || stateUser.user.email,
-        // description: appData.value.description || '',
-        // description_short: appData.value.description_short || '',
-        // website_url: appData.value.website_url || '',
-    }
+
+const { handleSubmit, isSubmitting: isAppDesignSubmitting } = useForm({
+    initialValues: props.appData
 });
 
+const bg_color = useField('bg_color');
+const bg_color_gradient = useField('bg_color_gradient');
+const font_color = useField('font_color');
+const text_secondary_color = useField('text_secondary_color');
 
+const button_text_color = useField('button_text_color');
 const tabs_color = useField('tabs_color');
 const tabs_icon_color = useField('tabs_icon_color');
 const tabs_icon_selected_color = useField('tabs_icon_selected_color');
 const main_theme_color = useField('main_theme_color');
-const text_secondary_color = useField('text_secondary_color');
 const play_button_border_color = useField('play_button_border_color');
 const volume_buttons_color = useField('volume_buttons_color');
 const volume_bar_active_color = useField('volume_bar_active_color');
 const volume_bar_inactive_color = useField('volume_bar_inactive_color');
-const bg_color = useField('bg_color');
-const bg_color_gradient = useField('bg_color_gradient');
-const font_color = useField('font_color');
+
 
 </script>
+
+<style scoped>
+.sticky-top {
+    position: sticky;
+    top: 10px;
+}</style>
--- components/AppInfo.vue
+++ components/AppInfo.vue
@@ -1,67 +1,71 @@
 <template>
-<v-row no-gutters md="12">
-    <v-col md="12">
+    <v-container>
+        <v-row no-gutters md="12">
+            <v-col md="12">
 
-        <v-form @submit.prevent="onAppSubmit" :disabled="isAppInfoSubmitting">
-            <v-text-field v-model="title.value.value" type="text" :error-messages="title.errorMessage.value"
-                :label="$t('app.title')" maxlength="30"></v-text-field>
+                <v-form @submit.prevent="onAppSubmit" :disabled="isAppInfoSubmitting">
+                    <v-text-field v-model="title.value.value" type="text" :error-messages="title.errorMessage.value"
+                        :label="$t('app.title')" maxlength="30"></v-text-field>
 
-            <v-text-field v-if="isAndroid()" v-model="description_short.value.value" type="text"
-                :error-messages="description_short.errorMessage.value" :label="$t('app.description_short')"
-                :hint="$t('app.description_short_hint')" persistent-hint maxlength="80"></v-text-field>
+                    <v-text-field v-if="isAndroid()" v-model="description_short.value.value" type="text"
+                        :error-messages="description_short.errorMessage.value" :label="$t('app.description_short')"
+                        :hint="$t('app.description_short_hint')" persistent-hint maxlength="80"></v-text-field>
 
-            <v-textarea v-model="description.value.value" :label="$t('app.description')"
-                :error-messages="description.errorMessage.value" :hint="$t('app.description_hint')"
-                persistent-hint></v-textarea>
+                    <v-textarea v-model="description.value.value" :label="$t('app.description')"
+                        :error-messages="description.errorMessage.value" :hint="$t('app.description_hint')"
+                        persistent-hint></v-textarea>
 
-            <v-text-field v-model="website_url.value.value" type="url"
-                :error-messages="website_url.errorMessage.value" name="website_url"
-                :label="$t('app.website_url_label')"></v-text-field>
+                    <v-text-field v-model="website_url.value.value" type="url"
+                        :error-messages="website_url.errorMessage.value" name="website_url"
+                        :label="$t('app.website_url_label')"></v-text-field>
 
-            <v-text-field v-model="email.value.value" type="email"
-                :error-messages="email.errorMessage.value" :label="$t('email')"></v-text-field>
+                    <v-text-field v-model="email.value.value" type="email" :error-messages="email.errorMessage.value"
+                        :label="$t('email')"></v-text-field>
 
-            <v-file-input prepend-icon="mdi-account-box-outline" show-size :label="$t('app.icon')"
-                @change="generateIconPreview()" @click:clear="generateIconPreview()" name="icon"
-                v-model="icon.value.value" :error-messages="icon.errorMessage.value" accept="image/png"
-                :hint="$t('app.icon_hint') + icon_resolution + $t('app.icon_pixels')" persistent-hint>
-            </v-file-input>
+                    <v-file-input prepend-icon="mdi-account-box-outline" show-size :label="$t('app.icon')"
+                        @change="generateIconPreview()" @click:clear="generateIconPreview()" name="icon"
+                        v-model="icon.value.value" :error-messages="icon.errorMessage.value" accept="image/png"
+                        :hint="$t('app.icon_hint') + icon_resolution + $t('app.icon_pixels')" persistent-hint>
+                    </v-file-input>
 
-            <v-row>
-                <v-col cols="6" class="text-center">
-                    <img v-if="appData.icon || previewIcon" :src="previewIcon ? previewIcon : appData.icon" class="app-image-preview" />
-                    <v-icon v-else icon="mdi-tooltip-image-outline" size="x-large"
-                        style="font-size: 300px;"></v-icon>
-                </v-col>
-                <v-col cols="6" class="text-center">
-                    <img src="/img/app_icon.png" :alt="$t('app.icon')" />
-                </v-col>
-            </v-row>
-
-
-
-            <v-file-input prepend-icon="mdi-image" show-size :label="$t('app.logo')"
-                @change="generateLogoPreview()" @click:clear="generateLogoPreview()" name="logo"
-                v-model="logo.value.value" :error-messages="logo.errorMessage.value" accept="image/png"
-                :hint="$t('app.logo_hint')" persistent-hint></v-file-input>
-
-            <v-row>
-                <v-col cols="6" class="text-center">
-                    <img v-if="appData.logo || previewLogo" :src="previewLogo? previewLogo : appData.logo" class="app-image-preview" />
-                    <v-icon v-else icon="mdi-tooltip-image-outline" size="x-large"
-                        style="font-size: 300px;"></v-icon>
-                </v-col>
-                <v-col cols="6" class="text-center">
-                    <img src="/img/app_logo.png" :alt="$t('app.logo')" />
-                </v-col>
-            </v-row>
+                    <v-row>
+                        <v-col cols="6" class="text-center">
+                            <img v-if="appData.icon || previewIcon" :src="previewIcon ? previewIcon : appData.icon"
+                                class="app-image-preview" />
+                            <v-icon v-else icon="mdi-tooltip-image-outline" size="x-large"
+                                style="font-size: 300px;"></v-icon>
+                        </v-col>
+                        <v-col cols="6" class="text-center">
+                            <img src="/img/app_icon.png" :alt="$t('app.icon')" />
+                        </v-col>
+                    </v-row>
 
 
-            <v-btn type="submit" :disabled="isAppInfoSubmitting" block class="mt-2" color="primary">{{
-                isAppInfoSubmitting ? $t('loading') : $t('save') }}</v-btn>
-        </v-form>
-    </v-col>
-</v-row>
+
+                    <v-file-input prepend-icon="mdi-image" show-size :label="$t('app.logo')" @change="generateLogoPreview()"
+                        @click:clear="generateLogoPreview()" name="logo" v-model="logo.value.value"
+                        :error-messages="logo.errorMessage.value" accept="image/png" :hint="$t('app.logo_hint')"
+                        persistent-hint></v-file-input>
+
+                    <v-row>
+                        <v-col cols="6" class="text-center">
+                            <img v-if="appData.logo || previewLogo" :src="previewLogo ? previewLogo : appData.logo"
+                                class="app-image-preview" />
+                            <v-icon v-else icon="mdi-tooltip-image-outline" size="x-large"
+                                style="font-size: 300px;"></v-icon>
+                        </v-col>
+                        <v-col cols="6" class="text-center">
+                            <img src="/img/app_logo.png" :alt="$t('app.logo')" />
+                        </v-col>
+                    </v-row>
+
+
+                    <v-btn type="submit" :disabled="isAppInfoSubmitting" block class="mt-2" color="primary">{{
+                        isAppInfoSubmitting ? $t('loading') : $t('save') }}</v-btn>
+                </v-form>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script setup>
