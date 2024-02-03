04afe15d1911407ba9aa0be028d7eddab820cc93
Alexey V Paramonov <me@paramonov.ru>
2024-02-03 18:18:00 +0300

    App radio removal confirmation

--- components/AppRadios.vue
+++ components/AppRadios.vue
@@ -24,7 +24,7 @@
                     </thead>
                     <tbody>
 
-                        <tr v-if="appRadios.length > 0" v-for="appRadio in appRadios" :key="appRadio.id">
+                        <tr v-if="appRadios.length > 0 && !pending" v-for="appRadio in appRadios" :key="appRadio.id">
                             <td style="width: 100px;">
                                 <img :src="appRadio.logo" class="app-image-thumbnail">
                             </td>
@@ -34,10 +34,11 @@
                             </td>
                             <td>
                                 <v-btn icon="mdi-pencil"></v-btn>
+                                <v-btn icon="mdi-delete" @click="deleteRadio(appRadio)"></v-btn>
                             </td>
                         </tr>
 
-                        <tr v-else-if="radios_loading">
+                        <tr v-else-if="pending">
                             <td colspan="10" class="text-center"><v-progress-circular indeterminate></v-progress-circular>
                             </td>
                         </tr>
@@ -94,8 +95,7 @@
                         <v-col cols="6" class="text-center">
                             <img v-if="appRadio.logo || previewLogo" :src="previewLogo ? previewLogo : appRadio.logo"
                                 class="app-image-preview" />
-                            <v-icon v-else icon="mdi-camera" size="x-large"
-                                style="font-size: 100px;"></v-icon>
+                            <v-icon v-else icon="mdi-camera" size="x-large" style="font-size: 100px;"></v-icon>
                         </v-col>
                         <v-col cols="6">
                             <v-file-input prepend-icon="mdi-image" show-size :label="$t('app.radio.logo')"
@@ -135,6 +135,43 @@
         </v-card>
         <v-btn color="primary" block @click="radioDialog = false">{{ $t('close') }}</v-btn>
     </v-dialog>
+
+    <v-dialog v-model="delDialog" width="auto">
+        <v-card>
+            <v-card-text>
+                <div class="py-12 text-center">
+                    <v-icon class="mb-6" color="error" icon="mdi-alert-decagram" size="128"></v-icon>
+
+                    <div class="text-h4 font-weight-bold">{{ $t('app.radios.delete_confirmation') }}</div>
+                </div>
+            </v-card-text>
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn variant="text" color="grey" @click="answerDialog(false)">{{ $t('cancel') }}</v-btn>
+                <v-btn variant="outlined" color="primary" @click="answerDialog(true)">{{ $t('delete') }}</v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+
+    <v-snackbar v-model="deleteRadioFailed" color="error">
+        {{ $t('app.radios.delete_failed') }}
+
+        <template v-slot:actions>
+            <v-btn color="white" variant="text" @click="deleteRadioFailed = false">
+                {{ $t('close') }}
+            </v-btn>
+        </template>
+    </v-snackbar>
+
+    <v-snackbar v-model="deleteRadioSuccess" color="success">
+        {{ $t('app.radios.delete_success') }}
+
+        <template v-slot:actions>
+            <v-btn color="white" variant="text" @click="deleteRadioSuccess = false">
+                {{ $t('close') }}
+            </v-btn>
+        </template>
+    </v-snackbar>
 </template>
 
 <script setup>
@@ -150,6 +187,11 @@ let radioDialog = ref(false);
 // let appRadios = ref([]);
 let scRadios = ref([]);
 const appRadio = ref({});
+const delDialog = ref(false);
+let answerDialog = ref();
+const deleteRadioSuccess = ref(false);
+const deleteRadioFailed = ref(false);
+
 
 const previewLogo = ref();
 
@@ -176,12 +218,8 @@ allow_likes.value.value = "1";
 allow_dislikes.value.value = "1";
 
 const { data: appRadios, pending, error, refresh } = await useFetchAuth(`${config.public.baseURL}/mobile_apps/radio/${props.platform}/`);
-console.log("APP RADIOS: ", appRadios);
 
 
-function isAndroid() {
-    return props.platform == 'android';
-}
 function setRadioData(v) {
     const id = v || sc_server_id.value.value;
     let radio = scRadios.value.find(r => r.id === id);
@@ -192,9 +230,9 @@ function setRadioData(v) {
         description.value.value = radio.description;
     }
 }
+
 async function generateLogoPreview() {
     const { valid } = await logo.validate();
-
     previewLogo.value = valid && logo.value.value[0] ? URL.createObjectURL(logo.value.value[0]) : undefined;
 }
 
@@ -215,10 +253,30 @@ async function checkSCPanelURL() {
     setRadioData();
 }
 
+
+function deleteRadio(r) {
+    delDialog.value = true;
+    new Promise((resolve) => {
+        answerDialog = resolve;
+    }).then((res) => {
+        delDialog.value = false;
+        if (res) {
+            fetchAuth(`${config.public.baseURL}/mobile_apps/radio/${props.platform}/${r.id}/`, { method: 'DELETE' }).then(
+                () => {
+                    deleteRadioSuccess.value = true;
+                    refresh();
+                },
+                () => {
+                    deleteRadioFailed.value = true;
+                }
+            );
+        }
+    })
+}
+
 async function saveAppRadioRequest(values) {
-    const platform = isAndroid() ? "android" : "ios";
     let formData = new FormData();
-    
+
     formData.append('app', appData.id);
     formData.append('title', values.title);
     // formData.append('user', stateUser.user.id);
@@ -229,7 +287,7 @@ async function saveAppRadioRequest(values) {
     // values.icon && formData.append('icon', values.icon[0]);
     values.logo && formData.append('logo', values.logo[0]);
 
-    return await useFetchAuth(`${config.public.baseURL}/mobile_apps/radio/${platform}/`, {
+    return await useFetchAuth(`${config.public.baseURL}/mobile_apps/radio/${props.platform}/`, {
         method: 'POST',
         body: formData
     });
@@ -270,5 +328,4 @@ const onAppRadioSubmit = handleSubmit(async values => {
     max-height: 100px;
     max-width: 100px;
 }
-
 </style>
