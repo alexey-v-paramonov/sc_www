cef5de780c86d6853a65009972e0bbfef012a278
Alexey V Paramonov <me@paramonov.ru>
2024-04-23 13:25:32 +0300

    Option to remove the account

--- pages/settings/index.vue
+++ pages/settings/index.vue
@@ -19,9 +19,26 @@
 
           <v-checkbox v-model="subscribed.value.value" :label="$t('settings.subscribed')" type="checkbox"></v-checkbox>
 
+
           <v-btn type="submit" :disabled="isSettingsSubmitting" block class="mt-2" color="primary">{{
             isSettingsSubmitting ? $t('loading') : $t('save') }}</v-btn>
         </v-form>
+      </v-col>
+    </v-row>
+    <v-spacer>&nbsp;</v-spacer>
+    <v-spacer>&nbsp;</v-spacer>
+    <v-spacer>&nbsp;</v-spacer>
+    <v-row md="12">
+      <v-col>
+        <div class="text-h5">{{ $t('settings.delete_account_title') }}</div>
+        <p>{{ $t('settings.delete_account_title_text') }}</p>
+      </v-col>
+    </v-row>
+    <v-row md="12">
+      <v-col md="12">
+        <v-btn :disabled="isRemovalRequestRunning || stateUser.user.userData.balance != 0" block class="mt-2"
+          color="error" @click="sendRemovalRequest()">{{
+            isRemovalRequestRunning ? $t('loading') : $t('settings.delete_account_cta') }}</v-btn>
       </v-col>
     </v-row>
 
@@ -31,6 +48,27 @@
 
     <template v-slot:actions>
       <v-btn color="white" variant="text" @click="settingsSaveSuccess = false">
+        {{ $t('close') }}
+      </v-btn>
+    </template>
+  </v-snackbar>
+
+  <v-snackbar v-model="deleteRequestFailed" color="error">
+    {{ $t('settings.delete_account_request_failed') }}
+
+    <template v-slot:actions>
+      <v-btn color="white" variant="text" @click="deleteRequestFailed = false">
+        {{ $t('close') }}
+      </v-btn>
+    </template>
+  </v-snackbar>
+
+
+  <v-snackbar v-model="deleteRequestSuccess" color="success">
+    {{ $t('settings.delete_account_request_success') }}
+
+    <template v-slot:actions>
+      <v-btn color="white" variant="text" @click="deleteRequestSuccess = false">
         {{ $t('close') }}
       </v-btn>
     </template>
@@ -49,7 +87,11 @@ const { t } = useI18n();
 
 const stateUI = useUiStateStore()
 const showPass = ref(false);
-let settingsSaveSuccess = ref(false);
+const settingsSaveSuccess = ref(false);
+const isRemovalRequestRunning = ref(false);
+const config = useRuntimeConfig();
+let deleteRequestSuccess = ref(false);
+let deleteRequestFailed = ref(false);
 
 definePageMeta({
   layout: "default",
@@ -112,5 +154,20 @@ const onSettingsSubmit = handleSubmit(async values => {
   settingsSaveSuccess.value = true;
   stateUser.user.userData.subscribed = subscribed.value.value;
 });
+
+function sendRemovalRequest(app) {
+  isRemovalRequestRunning.value = true;
+
+  fetchAuth(`${config.public.baseURL}/users/${stateUser.user.id}/request_account_deletion/`, { method: 'POST' }).then(
+    (r) => {
+      deleteRequestSuccess.value = true;
+    },
+    (e) => {
+      deleteRequestFailed.value = true;
+    }
+  ).finally(() => {
+    isRemovalRequestRunning.value = false;
+  });
+}
 
 </script>
