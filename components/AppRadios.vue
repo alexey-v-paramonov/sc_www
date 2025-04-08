701283245227f78a7262cb5b8fd6aef93c3c9090
Alexey V Paramonov <me@paramonov.ru>
2025-04-09 00:06:44 +0300

    Fix url validator

--- components/AppRadios.vue
+++ components/AppRadios.vue
@@ -759,7 +759,7 @@ async function addSocialLink() {
     if (new_social_link_type.value.value !== 'phone' && 
             new_social_link_type.value.value !== 'email' && new_social_link_type.value.value !== 'whatsapp') {
         
-        const urlPattern = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w.-]*)*\/?$/;
+        const urlPattern = /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})).?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
         
         if (!urlPattern.test(new_social_link_value.value.value)) {
             setErrors({ ['new_social_link_value']: t('errors.url') });
